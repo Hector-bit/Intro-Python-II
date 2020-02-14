60fd7946de933f5e2e868650af90ab8416369765
Hector-bit <hectormartinez.2978@gmail.com>
2020-02-14 01:36:26 -0800

    picking up an item adds it inventory and removes it from the room

--- src/adv.py
+++ src/adv.py
@@ -5,21 +5,21 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-"North of you, the cave mount beckons"),
+"North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ['Pocket Watch']),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 # Link rooms together
 room['outside'].n_to = room['foyer']
@@ -31,11 +31,11 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 # room item
-room['outside'].item = None
-room['foyer'].item = 'Pocket Watch'
-room['overlook'].item = None
-room['narrow'].item = None
-room['treasure'].item = None
+# room['outside'].item = []
+# room['foyer'].item = ['Pocket Watch']
+# room['overlook'].item = []
+# room['narrow'].item = []
+# room['treasure'].item = []
 #
 # Main
 #
--- src/player.py
+++ src/player.py
@@ -16,18 +16,25 @@ class Player:
     def inventory_check(self):
         if len(self.inventory) == 0:
             print('You do not have any items in your inventory')
+        else:
+            print(self.inventory)
     #function look will find items in the room
     def look(self, room):
-        if self.current_room.item != None:
-            print(f'You found a {self.current_room.item} in the {self.current_room.name}')
+        if len(self.current_room.item) != 0:
+            print(f'You found a {self.current_room.item[0]} in the {self.current_room.name}')
         else:
             print('Nothing was found in this room')
     #pick up item will need to know which room it is in
     #order to see if there is an item available to pick up
     def pick_up_item(self, room):
-        picking_up = input("What item would you like to pick up: ").lower()
-        if picking_up == self.current_room.item.lower():
-            push self.current_room.item 
-            print(f"You picked up the {self.current_room.item}")
+        picking_up = input("What item would you like to pick up: ")
+        print(self.current_room.item, 'item of room')
+        if picking_up == self.current_room.item[0]:
+            self.inventory.insert(0, self.current_room.item[0])
+            player_grab = self.current_room.item.copy()
+            self.current_room.item.remove(player_grab[0]) 
+            print(f"You picked it up")
         else:
             print('That item does not appear to be here')
+    def drop_item(self, room):
+        print('does nothing up')
